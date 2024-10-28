6c3766c0f016d3b010a7967317deae08d2711b6b
Arjen Miedema <a.miedema@jc-electronics.nl>
2024-10-28 11:53:15 +0100

    Update codebase to use 2 spaces in *.ts and *.json
    To stick with best practices for JavaScript related languages both the
    TypeScript and JSON files are set to use two spaces instead of four.
    This is also defined in the `.editorconfig` file so all systems will
    handle this the same.

--- tests/base/utils/Cart.ts
+++ tests/base/utils/Cart.ts
@@ -1,26 +1,27 @@
-import { expect, Page } from '@playwright/test';
+import {expect, Page} from '@playwright/test';
 import slugs from '../fixtures/before/slugs.json';
 
-import productPageSelector from '../fixtures/during/selectors/product-page.json';
+import productPageSelector
+  from '../fixtures/during/selectors/product-page.json';
 import productPageExpected from '../fixtures/verify/expects/product-page.json';
 
 import miniCartSelector from '../fixtures/during/selectors/minicart.json';
 
 export class Cart {
-    page: Page;
+  page: Page;
 
-    constructor(page: Page) {
-        this.page = page;
-    }
+  constructor(page: Page) {
+    this.page = page;
+  }
 
-    async addSimpleProductToCart(productSlug: string) {
-        await this.page.goto(slugs.simpleProductSlug);
-        await this.page.click(productPageSelector.addToCartButtonSelector);
-        await expect(this.page.locator(`text=${productPageExpected.productAddedToCartNotificationText}`)).toBeVisible();
-    }
+  async addSimpleProductToCart(productSlug: string) {
+    await this.page.goto(slugs.simpleProductSlug);
+    await this.page.click(productPageSelector.addToCartButtonSelector);
+    await expect(this.page.locator(`text=${productPageExpected.productAddedToCartNotificationText}`)).toBeVisible();
+  }
 
-    async openMiniCart() {
-        await this.page.click(miniCartSelector.miniCartIconSelector);
-        await expect(this.page.locator(miniCartSelector.miniCartDrawerTitleSelector)).toBeVisible();
-    }
+  async openMiniCart() {
+    await this.page.click(miniCartSelector.miniCartIconSelector);
+    await expect(this.page.locator(miniCartSelector.miniCartDrawerTitleSelector)).toBeVisible();
+  }
 }
--- tests/base/utils/PageTester.ts
+++ tests/base/utils/PageTester.ts
@@ -1,57 +1,57 @@
-import { expect, Page } from '@playwright/test';
-import { Browser } from './Browser';
+import {expect, Page} from '@playwright/test';
+import {Browser} from './Browser';
 import globalExpect from '../fixtures/verify/expects/global.json';
 import flags from '../config/test-toggles.json';
 
 export class PageTester {
-    page: Page;
-    url: string;
-    errorHandler: Browser;
-    errors: Object[];
+  page: Page;
+  url: string;
+  errorHandler: Browser;
+  errors: Object[];
 
-    constructor(page: Page, url: string) {
-        this.page = page;
-        this.url = url;
-        this.errorHandler = new Browser(page);
-        this.errors = [];
-    }
-    
-    async navigateAndCheckStatus() {
-        const response = await this.page.goto(this.url);
-        expect(response?.status()).toBe(200);
-    }
+  constructor(page: Page, url: string) {
+    this.page = page;
+    this.url = url;
+    this.errorHandler = new Browser(page);
+    this.errors = [];
+  }
 
-    async waitAndCheckForErrors(timeout = 2000) {
-        await this.page.waitForTimeout(timeout);
-        await this.captureConsoleErrors();
-        const errors = this.errorHandler.getErrors();
-        this.errors = this.errors.concat(errors);
-        expect(this.errors).toHaveLength(0);
-    }
+  async navigateAndCheckStatus() {
+    const response = await this.page.goto(this.url);
+    expect(response?.status()).toBe(200);
+  }
 
-    async captureConsoleErrors() {
-        this.page.on('console', (msg) => {
-            if (msg.type() === 'error' || msg.type() === 'warning') {
-                this.errors.push({
-                    message: msg.text(),
-                    location: msg.location(),
-                    type: msg.type()
-                });
-            }
+  async waitAndCheckForErrors(timeout = 2000) {
+    await this.page.waitForTimeout(timeout);
+    await this.captureConsoleErrors();
+    const errors = this.errorHandler.getErrors();
+    this.errors = this.errors.concat(errors);
+    expect(this.errors).toHaveLength(0);
+  }
+
+  async captureConsoleErrors() {
+    this.page.on('console', (msg) => {
+      if (msg.type() === 'error' || msg.type() === 'warning') {
+        this.errors.push({
+          message: msg.text(),
+          location: msg.location(),
+          type: msg.type()
         });
-    }
+      }
+    });
+  }
 
-    async measurePerformance() {
-        const timing = JSON.parse(await this.page.evaluate(() => JSON.stringify(window.performance.timing)));
-        const loadTime = timing.loadEventEnd - timing.navigationStart;
-        expect(loadTime).toBeLessThan(globalExpect.maxPageLoadTimeInMs);
-    }
+  async measurePerformance() {
+    const timing = JSON.parse(await this.page.evaluate(() => JSON.stringify(window.performance.timing)));
+    const loadTime = timing.loadEventEnd - timing.navigationStart;
+    expect(loadTime).toBeLessThan(globalExpect.maxPageLoadTimeInMs);
+  }
 
-    async testPage() {
-        if (flags.testForPageErrors) {
-            await this.navigateAndCheckStatus();
-            await this.waitAndCheckForErrors();
-            await this.measurePerformance();
-        }
+  async testPage() {
+    if (flags.testForPageErrors) {
+      await this.navigateAndCheckStatus();
+      await this.waitAndCheckForErrors();
+      await this.measurePerformance();
     }
+  }
 }
