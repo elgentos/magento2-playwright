7fbb3d85510f933834a75af4f4c5e1ea6a650940
Shay <shayfaber@gmail.com>
2024-11-06 16:13:59 +0100

    Add Gherkin description to 'Test minicart to checkout'

--- tests/base/minicart.spec.ts
+++ tests/base/minicart.spec.ts
@@ -9,6 +9,19 @@ import miniCartSelector from './fixtures/during/selectors/minicart.json';
 import miniCartExpected from './fixtures/verify/expects/minicart.json';
 
 if (toggle.minicart.testMiniCartCheckoutButton) {
+  /**
+   * @feature Magento 2 Minicart to Checkout
+   *  @scenario User adds a product to their cart, then navigates to checkout through the minicart
+   *    @given I am on any Magento 2 page
+   *    @when I go to a Simple Product page
+   *      @and I add it to my cart
+   *      @then I should see a message to confirm it was added
+   *    @when I click on the shopping cart symbol in the main menu
+   *      @then I should see a mini cart title
+   *    @when I click on 'Checkout'
+   *    @then I should navigate to the checkout page with the product in my cart
+   */
+
   test('Test minicart to checkout', async ({page}) => {
     const cart = new Cart(page);
     await cart.addSimpleProductToCart(slugs.simpleProductSlug);
@@ -18,6 +31,8 @@ if (toggle.minicart.testMiniCartCheckoutButton) {
     await expect(page).toHaveURL(new RegExp(`${slugs.checkoutSlug}.*`));
   });
 }
+
+
 
 if (toggle.minicart.testMiniCartLink) {
   test('Test minicart to cart', async ({page}) => {
