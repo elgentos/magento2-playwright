a182eaafc68ef4307dd6ef99ff1ede1c52a872ab
Shay <shayfaber@gmail.com>
2024-12-10 11:40:17 +0100

    Add test to remove product from Cart page

--- tests/base/cart.spec.ts
+++ tests/base/cart.spec.ts
@@ -1,11 +1,13 @@
-import {test, expect} from '@playwright/test';
-import {ProductPage} from './fixtures/product.page';
+import { test, expect } from '@playwright/test';
+import { ProductPage } from './fixtures/product.page';
 import { MainMenuPage } from './fixtures/mainmenu.page';
+import { CartPage } from './fixtures/cart.page';
 
 import slugs from './config/slugs.json';
+import selectors from './config/selectors/selectors.json';
 import verify from './config/expected/expected.json';
 
-test.describe('Coupon Code tests', () => {
+test.describe('Cart functionalities', () => {
   /**
    * @feature BeforeEach runs before each test in this group.
    * @scenario Add a product to the cart and confirm it's there.
@@ -31,6 +33,20 @@ test.describe('Coupon Code tests', () => {
 
   test('Add coupon code in cart',{ tag: ['@cart', '@coupon-code']}, async ({page}) => {
     
+  });
+
+  /**
+   * @feature Remove product from cart
+   * @scenario User has added a product and wants to remove it from the cart page
+   * @given I have added a product to my cart
+   *  @and I am on the cart page
+   * @when I click the delete button
+   * @then I should see a notification that the product has been removed from my cart
+   *  @and I should no longer see the product in my cart
+   */
+  test('Remove product from cart',{ tag: '@cart',}, async ({page}) => {
+    const cart = new CartPage(page);
+    await cart.removeProduct(selectors.productPage.simpleProductTitle);
   });
 });
 
--- tests/base/fixtures/cart.page.ts
+++ tests/base/fixtures/cart.page.ts
@@ -1,5 +1,6 @@
 import {expect, type Locator, type Page} from '@playwright/test';
 
+import selectors from '../config/selectors/selectors.json';
 
 export class CartPage {
   readonly page: Page;
@@ -7,6 +8,12 @@ export class CartPage {
 
   constructor(page: Page) {
     this.page = page;
-    this.applyDiscountButton = this.page.getByRole('button', { name: 'Apply Discount Code' });
+    this.applyDiscountButton = this.page.getByRole('button', { name: selectors.cart.applyDiscountCodeLabel });
+  }
+
+  async removeProduct(name: string){
+    let removeButton = this.page.getByLabel(`${selectors.cart.remove} ${name}`);
+    await removeButton.click();
+    await expect(removeButton,`Button to remove product is no longer visible`).toBeHidden();
   }
 }
