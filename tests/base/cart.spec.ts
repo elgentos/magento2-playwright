5eb64d1c7463c14b81d59cde6c52d63a630a8180
Shay <shayfaber@gmail.com>
2024-12-10 14:14:11 +0100

    Tests to add and remove coupon code from cart

--- tests/base/cart.spec.ts
+++ tests/base/cart.spec.ts
@@ -1,6 +1,7 @@
 import {test, expect} from '@playwright/test';
 import {ProductPage} from './fixtures/product.page';
 import { MainMenuPage } from './fixtures/mainmenu.page';
+import { CartPage} from './fixtures/cart.page';
 
 import slugs from './config/slugs.json';
 import verify from './config/expected/expected.json';
@@ -29,10 +30,54 @@ test.describe('Coupon Code tests', () => {
     await page.goto(slugs.cartSlug);
   });
 
+  /**
+   * @feature Discount Code
+   * @scenario User adds a discount code to their cart
+   * @given I have a product in my cart
+   *  @and I am on my cart page
+   * @when I click on the 'add discount code' button
+   * @then I fill in a code
+   *  @and I click on 'apply code'
+   * @then I should see a confirmation that my code has been added
+   *  @and the code should be visible in the cart
+   *  @and a discount should be applied to the product
+   */
   test('Add coupon code in cart',{ tag: ['@cart', '@coupon-code']}, async ({page}) => {
-    
-  });
-});
+    const cart = new CartPage(page);
+    let discountCode = process.env.DISCOUNT_CODE;
 
-//TODO: Write test to add coupon
-//TODO: Write test to remove coupon
+    if(!discountCode) {
+      throw new Error(`discountCode appears to not be set in .env file. Value reported: ${discountCode}`);
+    }
+
+    await cart.applyDiscountCode(discountCode);
+  });
+
+  /**
+   * @feature Remove discount code from cart
+   * @scenario User has added a discount code, then removes it
+   * @given I have a product in my cart
+   * @and I am on my cart page
+   * @when I add a discount code
+   * @then I should see a notification
+   * @and the code should be visible in the cart
+   * @and a discount should be applied to a product
+   * @when I click the 'cancel coupon' button
+   * @then I should see a notification the discount has been removed
+   * @and the discount should no longer be visible.
+   */
+
+  test('Remove coupon code from cart',{ tag: ['@cart', '@coupon-code']}, async ({page}) => {
+    const cart = new CartPage(page);
+    let discountCode = process.env.DISCOUNT_CODE;
+
+    if(!discountCode) {
+      throw new Error(`discountCode appears to not be set in .env file. Value reported: ${discountCode}`);
+    }
+
+    // TODO: create API call to quickly add discount code rather than run a test again.
+    await cart.applyDiscountCode(discountCode);
+    await cart.removeDiscountCode();
+  });
+
+});
--- tests/base/fixtures/cart.page.ts
+++ tests/base/fixtures/cart.page.ts
@@ -1,12 +1,42 @@
 import {expect, type Locator, type Page} from '@playwright/test';
 
+import selectors from '../config/selectors/selectors.json';
+import verify from '../config/expected/expected.json';
 
 export class CartPage {
   readonly page: Page;
-  readonly applyDiscountButton: Locator;
+  readonly showDiscountButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.applyDiscountButton = this.page.getByRole('button', { name: 'Apply Discount Code' });
+    this.showDiscountButton = this.page.getByRole('button', { name: selectors.cart.showDiscountFormButtonLabel });
+  }
+
+  async applyDiscountCode(code: string){
+    if(await this.page.getByPlaceholder(selectors.cart.discountInputFieldLabel).isHidden()){
+      // discount field is not open.
+      await this.showDiscountButton.click();
+    }
+    
+    let applyDiscoundButton = this.page.getByRole('button', {name: selectors.cart.applyDiscountButtonLabel, exact:true});
+    let discountField = this.page.getByPlaceholder(selectors.cart.discountInputFieldLabel);
+    await discountField.fill(code);
+    await applyDiscoundButton.click();
+    
+    await expect(this.page.getByText(`${verify.cart.discountAppliedNotification} "${code}"`),`Notification that discount code ${code} has been applied`).toBeVisible();
+    await expect(this.page.getByText(verify.cart.priceReducedSymbols),`'- $' should be visible on the page`).toBeVisible();
+  }
+
+  async removeDiscountCode(){
+    if(await this.page.getByPlaceholder(selectors.cart.discountInputFieldLabel).isHidden()){
+      // discount field is not open.
+      await this.showDiscountButton.click();
+    }
+  
+    let cancelCouponButton = this.page.getByRole('button', {name: selectors.cart.cancelCouponButtonLabel});
+    await cancelCouponButton.click();
+
+    await expect(this.page.getByText(verify.cart.discountRemovedNotification),`Notification should be visible`).toBeVisible();
+    await expect(this.page.getByText(verify.cart.priceReducedSymbols),`'- $' should not be on the page`).toBeHidden();
   }
 }
