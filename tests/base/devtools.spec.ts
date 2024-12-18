99ea764d350989632f1202b90c2da5931b2d5bfd
Shay <shayfaber@gmail.com>
2024-12-18 15:48:57 +0100

    WIP: worked out logic for tests in comment

--- tests/base/devtools.spec.ts
+++ tests/base/devtools.spec.ts
@@ -4,8 +4,20 @@ import {ProductPage} from './fixtures/product.page';
 import slugs from './config/slugs.json';
 import selectors from './config/selectors/selectors.json';
 
-base.describe.serial('Price checking tests', () => {
-  base('product input to cart is consistent from PDP to checkout', async ({page}) => {
+//TODO: Write gherkin feature descriptions
+base.describe('Price checking tests', () => {
+  //TODO: Rewrite this test as follows:
+  // Test: Simple Product Input check from PDP to checkout
+  // test.step: add simple product to cart, return priceOnPDP and productAmount as variables
+  // test.step: call function retrieveCheckoutPrices() to go to checkout, retrieve values
+  // test.step: call function compareRetrievedPrices() to compare price on PDP to price in checkout
+  
+  // Test: Configurable Product Input check from PDP to checkout
+  // test.step: add configurable product to cart, return priceOnPDP and productAmount as variables
+  // test.step: call function retrieveCheckoutPrices() to go to checkout, retrieve values
+  // test.step: call function compareRetrievedPrices() to compare price on PDP to price in checkout
+
+  base('Simple product input to cart is consistent from PDP to checkout', async ({page}) => {
     var productPagePrice: string;
     var productPageAmount: string;
     var productQuantityInCheckout: string;
