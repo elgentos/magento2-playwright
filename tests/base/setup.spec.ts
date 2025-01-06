a848e3692fc8379a5ea61ffd6189bbcadef6b38e
Shay <shayfaber@gmail.com>
2025-01-06 12:20:29 +0100

    Divided into two steps to ensure setup is done before env file is updated

--- tests/base/setup.spec.ts
+++ tests/base/setup.spec.ts
@@ -26,6 +26,68 @@ if (!process.env.SETUP_COMPLETE) {
   });
 
   base('Setup Magento environment for tests', async ({ page, browserName }) => {
+    await base.step(`Step 1: Perform actions`, async() =>{
+      const magentoAdminUsername = process.env.MAGENTO_ADMIN_USERNAME;
+      const magentoAdminPassword = process.env.MAGENTO_ADMIN_PASSWORD;
+  
+      if (!magentoAdminUsername || !magentoAdminPassword) {
+        throw new Error("MAGENTO_ADMIN_USERNAME or MAGENTO_ADMIN_PASSWORD is not defined in your .env file.");
+      }
+  
+      const magentoAdminPage = new MagentoAdminPage(page);
+      await magentoAdminPage.login(magentoAdminUsername, magentoAdminPassword);
+  
+      const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
+  
+      const couponCode = process.env[`MAGENTO_COUPON_CODE_${browserEngine}`];
+      if (!couponCode) {
+        throw new Error(`MAGENTO_COUPON_CODE_${browserEngine} is not defined in your .env file.`);
+      }
+      await magentoAdminPage.addCartPriceRule(couponCode);
+      await magentoAdminPage.disableLoginCaptcha();
+  
+      const accountPage = new AccountPage(page);
+  
+      const accountEmail = process.env[`MAGENTO_EXISTING_ACCOUNT_EMAIL_${browserEngine}`];
+      const accountPassword = process.env.MAGENTO_EXISTING_ACCOUNT_PASSWORD;
+  
+      if (!accountEmail || !accountPassword) {
+        throw new Error(
+          `MAGENTO_EXISTING_ACCOUNT_EMAIL_${browserEngine} or MAGENTO_EXISTING_ACCOUNT_PASSWORD is not defined in your .env file.`
+        );
+      }
+  
+      await accountPage.create(
+        values.accountCreation.firstNameValue,
+        values.accountCreation.lastNameValue,
+        accountEmail,
+        accountPassword
+      );
+    });
+    
+    await base.step(`Step 2: (optional) Update env file`, async() =>{
+      if (process.env.CI === 'true') {
+        console.log("Running in CI environment. Skipping .env update.");
+        process.exit(0);
+      }
+  
+      const envPath = path.resolve(__dirname, '../../.env');
+      try {
+        if (fs.existsSync(envPath)) {
+          const envContent = fs.readFileSync(envPath, 'utf-8');
+          if (!envContent.includes('SETUP_COMPLETE=true')) {
+            fs.appendFileSync(envPath, '\nSETUP_COMPLETE=true');
+            console.log("Environment setup completed successfully. 'SETUP_COMPLETE=true' added to .env file.");
+          }
+        } else {
+          throw new Error('.env file not found. Please ensure it exists in the root directory.');
+        }
+      } catch (error) {
+        throw new Error(`Failed to update .env file: ${error.message}`);
+      }
+    });
+
+    /*
     const magentoAdminUsername = process.env.MAGENTO_ADMIN_USERNAME;
     const magentoAdminPassword = process.env.MAGENTO_ADMIN_PASSWORD;
 
@@ -82,5 +144,7 @@ if (!process.env.SETUP_COMPLETE) {
     } catch (error) {
       throw new Error(`Failed to update .env file: ${error.message}`);
     }
+    */
   });
+
 }
