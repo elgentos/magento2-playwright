70dfbdf1ee289da4eb81d11447805be7c8857ddf
Shay <shayfaber@gmail.com>
2025-05-07 11:07:09 +0200

    Updated CI setup.spec.ts
    This commit updates the CI tests for `setup.spec.ts`
    to also contain the new test steps.

--- tests/base/setup.spec.ts
+++ tests/base/setup.spec.ts
@@ -26,16 +26,25 @@ if(process.env.CI) {
    * But NOT exclusively.
    */
   base.describe('Setting up the testing environment', () => {
-  
+    base.beforeEach('Check if setup is needed', async ({ browserName }, testInfo) => {
+      const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
+      const setupCompleteVar = `SETUP_COMPLETE_${browserEngine}`;
+      const isSetupComplete = process.env[setupCompleteVar];
+
+      if(isSetupComplete === 'DONE') {
+        testInfo.skip(true, `Skipping because configuration is only needed once.`);
+      }
+    });
+
     base('Enable multiple Magento admin logins', {tag: '@setup',}, async ({ page, browserName }, testInfo) => {
+      
       const magentoAdminUsername = process.env.MAGENTO_ADMIN_USERNAME;
       const magentoAdminPassword = process.env.MAGENTO_ADMIN_PASSWORD;
+      const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
   
       if (!magentoAdminUsername || !magentoAdminPassword) {
         throw new Error("MAGENTO_ADMIN_USERNAME or MAGENTO_ADMIN_PASSWORD is not defined in your .env file.");
-      }
-  
-      const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
+      }    
   
       /**
        * Only enable multiple admin logins for Chromium browser.
@@ -45,50 +54,58 @@ if(process.env.CI) {
         await magentoAdminPage.login(magentoAdminUsername, magentoAdminPassword);
         await magentoAdminPage.enableMultipleAdminLogins();
       } else {
-        testInfo.skip(true, `Skipping because configuration is only needed once.`);
+        testInfo.skip(true, `Skipping, configuration is only needed once. This step will only run in Chromium.`);
       }
     });
   
     base('Setup Magento environment for tests', {tag: '@setup',}, async ({ page, browserName }, testInfo) => {
+      // const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
+      // const setupCompleteVar = `SETUP_COMPLETE_${browserEngine}`;
+      // const isSetupComplete = process.env[setupCompleteVar];
+  
+      // if(isSetupComplete === 'DONE') {
+      //   testInfo.skip(true, `Skipping because configuration is only needed once.`);
+      // }
+
+      const magentoAdminPage = new MagentoAdminPage(page);
       const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
-      const setupCompleteVar = `SETUP_COMPLETE_${browserEngine}`;
-      const isSetupComplete = process.env[setupCompleteVar];
   
-      if(isSetupComplete === 'DONE') {
-        testInfo.skip(true, `Skipping because configuration is only needed once.`);
-      }
-  
-      await base.step(`Step 1: Perform actions`, async() =>{
+      await base.step('Step 1: login to admin', async () =>{
         const magentoAdminUsername = process.env.MAGENTO_ADMIN_USERNAME;
         const magentoAdminPassword = process.env.MAGENTO_ADMIN_PASSWORD;
   
         if (!magentoAdminUsername || !magentoAdminPassword) {
           throw new Error("MAGENTO_ADMIN_USERNAME or MAGENTO_ADMIN_PASSWORD is not defined in your .env file.");
         }
-  
-        const magentoAdminPage = new MagentoAdminPage(page);
+
+        // const magentoAdminPage = new MagentoAdminPage(page);
         await magentoAdminPage.login(magentoAdminUsername, magentoAdminPassword);
-  
-        const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
-  
+      });
+
+      await base.step('Step 2: Add cart price rules (coupon codes)', async () =>{
+        // const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
         const couponCode = process.env[`MAGENTO_COUPON_CODE_${browserEngine}`];
         if (!couponCode) {
           throw new Error(`MAGENTO_COUPON_CODE_${browserEngine} is not defined in your .env file.`);
         }
         await magentoAdminPage.addCartPriceRule(couponCode);
+      });
+
+      await base.step('Step 3: Disable login captcha', async () =>{
         await magentoAdminPage.disableLoginCaptcha();
-  
+      });
+
+      await base.step('Step 4: Create new account', async () =>{
         const registerPage = new RegisterPage(page);
-  
         const accountEmail = process.env[`MAGENTO_EXISTING_ACCOUNT_EMAIL_${browserEngine}`];
         const accountPassword = process.env.MAGENTO_EXISTING_ACCOUNT_PASSWORD;
-  
+
         if (!accountEmail || !accountPassword) {
           throw new Error(
             `MAGENTO_EXISTING_ACCOUNT_EMAIL_${browserEngine} or MAGENTO_EXISTING_ACCOUNT_PASSWORD is not defined in your .env file.`
           );
         }
-  
+
         await registerPage.createNewAccount(
           values.accountCreation.firstNameValue,
           values.accountCreation.lastNameValue,
@@ -96,7 +113,48 @@ if(process.env.CI) {
           accountPassword,
           true
         );
+
       });
+
+      // await base.step(`Step 1: Perform actions`, async() =>{
+      //   const magentoAdminUsername = process.env.MAGENTO_ADMIN_USERNAME;
+      //   const magentoAdminPassword = process.env.MAGENTO_ADMIN_PASSWORD;
+  
+      //   if (!magentoAdminUsername || !magentoAdminPassword) {
+      //     throw new Error("MAGENTO_ADMIN_USERNAME or MAGENTO_ADMIN_PASSWORD is not defined in your .env file.");
+      //   }
+  
+      //   const magentoAdminPage = new MagentoAdminPage(page);
+      //   await magentoAdminPage.login(magentoAdminUsername, magentoAdminPassword);
+  
+      //   const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
+  
+      //   const couponCode = process.env[`MAGENTO_COUPON_CODE_${browserEngine}`];
+      //   if (!couponCode) {
+      //     throw new Error(`MAGENTO_COUPON_CODE_${browserEngine} is not defined in your .env file.`);
+      //   }
+      //   await magentoAdminPage.addCartPriceRule(couponCode);
+      //   await magentoAdminPage.disableLoginCaptcha();
+  
+      //   const registerPage = new RegisterPage(page);
+  
+      //   const accountEmail = process.env[`MAGENTO_EXISTING_ACCOUNT_EMAIL_${browserEngine}`];
+      //   const accountPassword = process.env.MAGENTO_EXISTING_ACCOUNT_PASSWORD;
+  
+      //   if (!accountEmail || !accountPassword) {
+      //     throw new Error(
+      //       `MAGENTO_EXISTING_ACCOUNT_EMAIL_${browserEngine} or MAGENTO_EXISTING_ACCOUNT_PASSWORD is not defined in your .env file.`
+      //     );
+      //   }
+  
+      //   await registerPage.createNewAccount(
+      //     values.accountCreation.firstNameValue,
+      //     values.accountCreation.lastNameValue,
+      //     accountEmail,
+      //     accountPassword,
+      //     true
+      //   );
+      // });
   
       await base.step(`Step 2: (optional) Update env file`, async() =>{
         if (process.env.CI === 'true') {
@@ -126,6 +184,107 @@ if(process.env.CI) {
       });
     });
   });
+  // base.describe('Setting up the testing environment', () => {
+  
+  //   base('Enable multiple Magento admin logins', {tag: '@setup',}, async ({ page, browserName }, testInfo) => {
+  //     const magentoAdminUsername = process.env.MAGENTO_ADMIN_USERNAME;
+  //     const magentoAdminPassword = process.env.MAGENTO_ADMIN_PASSWORD;
+  
+  //     if (!magentoAdminUsername || !magentoAdminPassword) {
+  //       throw new Error("MAGENTO_ADMIN_USERNAME or MAGENTO_ADMIN_PASSWORD is not defined in your .env file.");
+  //     }
+  
+  //     const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
+  
+  //     /**
+  //      * Only enable multiple admin logins for Chromium browser.
+  //      */
+  //     if (browserEngine === "CHROMIUM") {
+  //       const magentoAdminPage = new MagentoAdminPage(page);
+  //       await magentoAdminPage.login(magentoAdminUsername, magentoAdminPassword);
+  //       await magentoAdminPage.enableMultipleAdminLogins();
+  //     } else {
+  //       testInfo.skip(true, `Skipping because configuration is only needed once.`);
+  //     }
+  //   });
+  
+  //   base('Setup Magento environment for tests', {tag: '@setup',}, async ({ page, browserName }, testInfo) => {
+  //     const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
+  //     const setupCompleteVar = `SETUP_COMPLETE_${browserEngine}`;
+  //     const isSetupComplete = process.env[setupCompleteVar];
+  
+  //     if(isSetupComplete === 'DONE') {
+  //       testInfo.skip(true, `Skipping because configuration is only needed once.`);
+  //     }
+  
+  //     await base.step(`Step 1: Perform actions`, async() =>{
+  //       const magentoAdminUsername = process.env.MAGENTO_ADMIN_USERNAME;
+  //       const magentoAdminPassword = process.env.MAGENTO_ADMIN_PASSWORD;
+  
+  //       if (!magentoAdminUsername || !magentoAdminPassword) {
+  //         throw new Error("MAGENTO_ADMIN_USERNAME or MAGENTO_ADMIN_PASSWORD is not defined in your .env file.");
+  //       }
+  
+  //       const magentoAdminPage = new MagentoAdminPage(page);
+  //       await magentoAdminPage.login(magentoAdminUsername, magentoAdminPassword);
+  
+  //       const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
+  
+  //       const couponCode = process.env[`MAGENTO_COUPON_CODE_${browserEngine}`];
+  //       if (!couponCode) {
+  //         throw new Error(`MAGENTO_COUPON_CODE_${browserEngine} is not defined in your .env file.`);
+  //       }
+  //       await magentoAdminPage.addCartPriceRule(couponCode);
+  //       await magentoAdminPage.disableLoginCaptcha();
+  
+  //       const registerPage = new RegisterPage(page);
+  
+  //       const accountEmail = process.env[`MAGENTO_EXISTING_ACCOUNT_EMAIL_${browserEngine}`];
+  //       const accountPassword = process.env.MAGENTO_EXISTING_ACCOUNT_PASSWORD;
+  
+  //       if (!accountEmail || !accountPassword) {
+  //         throw new Error(
+  //           `MAGENTO_EXISTING_ACCOUNT_EMAIL_${browserEngine} or MAGENTO_EXISTING_ACCOUNT_PASSWORD is not defined in your .env file.`
+  //         );
+  //       }
+  
+  //       await registerPage.createNewAccount(
+  //         values.accountCreation.firstNameValue,
+  //         values.accountCreation.lastNameValue,
+  //         accountEmail,
+  //         accountPassword,
+  //         true
+  //       );
+  //     });
+  
+  //     await base.step(`Step 2: (optional) Update env file`, async() =>{
+  //       if (process.env.CI === 'true') {
+  //         console.log("Running in CI environment. Skipping .env update.");
+  //         base.skip();
+  //       }
+  
+  //       const envPath = path.resolve(__dirname, '../../.env');
+  //       try {
+  //         if (fs.existsSync(envPath)) {
+  //           const envContent = fs.readFileSync(envPath, 'utf-8');
+  //           const browserEngine = browserName?.toUpperCase() || "UNKNOWN";
+  //           if (!envContent.includes(`SETUP_COMPLETE_${browserEngine}='DONE'`)) {
+  //             fs.appendFileSync(envPath, `\nSETUP_COMPLETE_${browserEngine}='DONE'`);
+  //             console.log(`Environment setup completed successfully. 'SETUP_COMPLETE_${browserEngine}='DONE'' added to .env file.`);
+  //           }
+  //           // if (!envContent.includes(`SETUP_COMPLETE_${browserEngine}=true`)) {
+  //           //   fs.appendFileSync(envPath, `\nSETUP_COMPLETE_${browserEngine}=true`);
+  //           //   console.log(`Environment setup completed successfully. 'SETUP_COMPLETE_${browserEngine}=true' added to .env file.`);
+  //           // }
+  //         } else {
+  //           throw new Error('.env file not found. Please ensure it exists in the root directory.');
+  //         }
+  //       } catch (error) {
+  //         throw new Error(`Failed to update .env file: ${error.message}`);
+  //       }
+  //     });
+  //   });
+  // });
 } else {
   if(toggles.general.setup) {
     /**
