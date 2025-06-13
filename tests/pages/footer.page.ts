c0f94ed434c7e6b28552d37723bcecd6e7f3a0e0
Myron <myron@elgentos.nl>
2025-06-13 16:12:12 +0200

    added uireference elements.

--- tests/pages/footer.page.ts
+++ tests/pages/footer.page.ts
@@ -2,6 +2,7 @@
 
 import { expect, Locator, type Page } from '@playwright/test';
 import { UIReference, outcomeMarker, inputValues } from 'config';
+import { findPackageJSON } from "node:module";
 
 export class Footer {
     readonly page: Page
@@ -19,17 +20,17 @@ export class Footer {
     async switchCurrencySwitcher () {
         await this.getFooterElement();
         await this.page.getByRole('button', { name: UIReference.footerPage.currencyDollar }).click();
-        await expect(this.page.getByRole('navigation', {name: 'Currency'})).toBeVisible();
+        await expect(this.page.getByRole('navigation', { name: UIReference.footerPage.currencyLabel })).toBeVisible();
         await this.page.getByRole('link', { name: UIReference.footerPage.currencyEuro }).click();
         await this.page.reload();
         await expect(this.page.getByRole('button', { name: UIReference.footerPage.currencyEuro })).toBeVisible();
     }
 
     async subscribeToNewsletter() {
-        await expect(this.page.getByRole('textbox', { name: 'Email Address' })).toBeVisible();
-        await this.page.getByRole('textbox', { name: 'Email Address' }).fill(inputValues.contact.contactFormEmailValue);
-        await this.page.getByRole('button', { name: 'Subscribe' }).click();
-        // await this.page.reload();
+        await expect(this.page.getByRole('textbox', { name: UIReference.footerPage.newsletterInputElementLabel })).toBeVisible();
+        await this.page.getByRole('textbox', { name: UIReference.footerPage.newsletterInputElementLabel }).fill(inputValues.contact.contactFormEmailValue);
+        await this.page.getByRole('button', { name: UIReference.footerPage.newsletterSubscribeButtonLabel }).click();
+
         await expect(this.page.getByText(outcomeMarker.footerPage.newsletterSubscription)).toBeVisible();
     }
 }
