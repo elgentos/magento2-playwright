23e5a02efb35d16e8c4dcb70fc87bc5adc79bb77
Shay <shayfaber@gmail.com>
2025-06-16 16:04:38 +0200

    Renamed install.js to build.js

--- build.js
+++ build.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 
 // destination will be created or overwritten by default.
-fs.copyFile('./test.example.ts', '../test.ts', (err) => {
+fs.copyFile('./playwright.config.example.ts', '../playwright.config.ts', (err) => {
   if (err) throw err;
   console.log('File was copied to destination');
 });
-
-console.log("Hello world");
