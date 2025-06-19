644ee5220d37e410c99c7ec79caf6e8273331cca
Myron <myron@elgentos.nl>
2025-06-19 11:40:38 +0200

    Updated build.js to class file. Updated existing code to better match file handling

--- playwright.config.example.ts
+++ playwright.config.example.ts
@@ -72,8 +72,12 @@ export default defineConfig({
     /* Ignore https errors if they apply (should only happen on local) */
     ignoreHTTPSErrors: true,
   },
-  /* Setup for global cookie to bypass CAPTCHA, remove '.example' when used */
-  // globalSetup: require.resolve('./bypass-captcha.config.example.ts'),
+
+  /*
+   * Setup for global cookie to bypass CAPTCHA, remove '.example' when used.
+   * If this is disabled remove storageState from all project objects.
+   */
+  globalSetup: require.resolve('./bypass-captcha.config.ts'),
 
   /* Configure projects for major browsers */
   projects: [
@@ -94,7 +98,8 @@ export default defineConfig({
       testMatch: testFiles,
       use: {
         ...devices['Desktop Firefox'],
-        storageState: './auth-storage/firefox-storage-state.json', },
+        storageState: './auth-storage/firefox-storage-state.json',
+      },
     },
 
     {
