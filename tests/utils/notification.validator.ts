69900bcdc609f1e957a31b8670e291e69b8e27d9
Myron <myron@elgentos.nl>
2025-07-01 15:49:03 +0200

    Update notification validator to accept notification type and value, and annotate test info with notification type and message

--- tests/utils/notification.validator.ts
+++ tests/utils/notification.validator.ts
@@ -14,10 +14,11 @@ class NotificationValidator {
     }
 
     /**
-     * @param value (expected text on page)
+     * @param notificationType
+     * @param value
      * @return json object
      */
-    async validate(value: string) {
+    async validate(notificationType: string, value: string) {
         await this.page.locator(UIReference.general.messageLocator).waitFor({ state: 'visible' });
         const notificationText = await this.page.locator(UIReference.general.messageLocator).textContent();
         let message = { success: true, message: 'Action was successful, but notification text could not be extracted.'};
@@ -34,7 +35,7 @@ class NotificationValidator {
             message = { success: false, message: `Notification text not found: ${value}. Found notification text: ${notificationText}` };
         }
 
-        this.testInfo.annotations.push({ type: 'Notification message on page', description: message.message });
+        this.testInfo.annotations.push({ type: `Notification: ${notificationType}`, description: message.message });
     }
 }
 
