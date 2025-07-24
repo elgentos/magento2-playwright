f1b3c3da9ddbab890375343c563e2c153ef652f2
Myron <myron@elgentos.nl>
2025-07-24 15:14:47 +0200

    now searches for the config files and merges them

--- tests/config/index.ts
+++ tests/config/index.ts
@@ -1,7 +1,37 @@
-// @ts-check
+import fs from 'fs';
+import path from 'path';
 
-export { default as UIReference } from './element-identifiers.json';
-export { default as outcomeMarker } from './outcome-markers.json';
-export { default as inputValues } from './input-values.json';
-export { default as slugs } from './slugs.json';
-export { default as toggles } from './test-toggles.json';
+function deepMerge(target: any, source: any): any {
+  for (const key in source) {
+	if (source[key] instanceof Object && key in target) {
+	  Object.assign(source[key], deepMerge(target[key], source[key]));
+	}
+  }
+  // Combine the two objects
+  return { ...target, ...source };
+}
+
+function loadAndMergeConfig(fileName: string) {
+  const fallbackPath = path.resolve(__dirname, fileName);
+  const currentPath = path.resolve(__dirname, '../../tests/config/', fileName);
+
+  let currentConfig = {};
+  let fallbackConfig = {};
+
+  if (fs.existsSync(currentPath)) {
+	currentConfig = JSON.parse(fs.readFileSync(currentPath, 'utf-8'));
+  }
+
+  if (fs.existsSync(fallbackPath)) {
+	fallbackConfig = JSON.parse(fs.readFileSync(fallbackPath, 'utf-8'));
+  }
+
+  // Use deepMerge instead of shallow merge
+  return deepMerge(fallbackConfig, currentConfig);
+}
+
+export const UIReference = loadAndMergeConfig('element-identifiers.json');
+export const outcomeMarker = loadAndMergeConfig('outcome-markers.json');
+export const inputValues = loadAndMergeConfig('input-values.json');
+export const slugs = loadAndMergeConfig('slugs.json');
+export const toggles = loadAndMergeConfig('test-toggles.json');
