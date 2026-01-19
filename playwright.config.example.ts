04486f88f6ef0d6bb11ef8abe3c510a4f91e3afd
Daniël Heesen <daheesen@gmail.com>
2026-01-19 20:06:23 +0100

    Removed faulthy config value

--- playwright.config.example.ts
+++ playwright.config.example.ts
@@ -62,8 +62,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Increase default timeout */
   timeout: 150_000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
