33ecd3b6fa1da7210f63034e63122aaf6ce9d036
Shay <shayfaber@gmail.com>
2026-05-27 11:58:50 +0200

    Set currency_switcher test in footer to fixme due to caching issues

--- tests/footer.spec.ts
+++ tests/footer.spec.ts
@@ -10,32 +10,27 @@ import Footer from '@poms/frontend/footer.page';
 
 test.describe('Footer', () => {
 
-	test(
-		'Footer_is_available',
-		{tag: ['@footer', '@cold']},
-		async ({page}) => {
+	test('Footer_is_available', {tag: ['@footer', '@cold']}, async ({page}) => {
 			const footer = new Footer(page);
 
 			await page.goto('');
 			await footer.goToFooterElement();
-		}
-	)
+	});
 
-	test(
-		'Footer_switch_currency',
-		{tag: ['@footer', '@cold']},
-		async ({page}) => {
+	/**
+	 * May 27th, 2026 - this test is set to fixme:
+	 * Caching issues on the demo website causes issues for the currency switching fuctionality.
+	 * This means this test fails and causes the entire suite to fail.
+	 * Unset 'fixme' when caching issue has been resolved.
+	 */
+	test.fixme('Footer_switch_currency', {tag: ['@footer', '@cold']}, async ({page}) => {
 			const footer = new Footer(page);
 
 			await page.goto('');
 			await footer.switchCurrency();
-		}
-	)
+	});
 
-	test(
-		'Footer_newsletter_subscription',
-		{tag: ['@footer', '@cold']},
-		async ({page}, testInfo) => {
+	test('Footer_newsletter_subscription', {tag: ['@footer', '@cold']}, async ({page}, testInfo) => {
 			const newsletterPage = new NewsletterPage(page);
 
 			await page.goto(requireEnv('PLAYWRIGHT_BASE_URL'));
@@ -46,6 +41,5 @@ test.describe('Footer', () => {
 
 			const notificationValidator = new NotificationValidatorUtils(page, testInfo);
 			await notificationValidator.validate(subscriptionOutput);
-		}
-	)
+	});
 })
