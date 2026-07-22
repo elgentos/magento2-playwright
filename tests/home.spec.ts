39d9b9e0b05faab16e8496faa559fdd16a6f5449
Shay <shayfaber@gmail.com>
2026-07-22 10:03:49 +0200

    wip: reworked locators for home.page.ts

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -4,13 +4,13 @@ import { test, expect } from '@playwright/test';
 import { outcomeMarker } from '@config';
 
 import MainMenuPage from '@poms/frontend/mainmenu.page';
-import HomePage from '@poms/frontend/home.page';
+import { BaseHomePage } from '@poms/frontend/home.page';
 
 test('Add_product_on_homepage_to_cart',{ tag: ['@homepage', '@cold']}, async ({page}) => {
-  const homepage = new HomePage(page);
+  const homepage = new BaseHomePage(page);
   const mainmenu = new MainMenuPage(page);
 
-  await page.goto('');
+  await homepage.goToHomePage();
   await homepage.addHomepageProductToCart();
   await mainmenu.openMiniCart();
   await expect(page.getByText('x ' + outcomeMarker.homePage.firstProductName), 'product should be visible in cart').toBeVisible();
