c9b7e06c66fc099b3d49997f7e55bc5adfaad467
Shay <shayfaber@gmail.com>
2026-08-04 10:02:05 +0200

    standardized locators in orderhistory.spec

--- tests/orderhistory.spec.ts
+++ tests/orderhistory.spec.ts
@@ -11,8 +11,8 @@ import { test } from '@playwright/test';
 
 import { BaseLoginPage } from '@poms/frontend/login.page';
 import { BaseProductPage } from '@poms/frontend/product.page';
-import CheckoutPage from '@poms/frontend/checkout.page';
-import OrderHistoryPage from '@poms/frontend/orderhistory.page';
+import { BaseCheckoutPage } from '@poms/frontend/checkout.page';
+import { BaseOrderHistoryPage } from '@poms/frontend/orderhistory.page';
 
 import { requireEnv } from '@utils/env.utils';
 import { UIReference, slugs } from '@config';
@@ -28,8 +28,8 @@ test('Recent_order_is_visible_in_history', async ({ page }) => {
 
 	const loginPage = new BaseLoginPage(page);
 	const productPage = new BaseProductPage(page);
-	const checkoutPage = new CheckoutPage(page);
-	const orderHistoryPage = new OrderHistoryPage(page);
+	const checkoutPage = new BaseCheckoutPage(page);
+	const orderHistoryPage = new BaseOrderHistoryPage(page);
 
 	await loginPage.goToLoginPage();
 	await loginPage.login(email, password);
